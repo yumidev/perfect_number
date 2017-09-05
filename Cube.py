aa06e12d16d0dc57161a0d0d5ffcae5646bf1ced
YUMIMAC <yumimac@YUMIDEV.local>
2017-09-05 18:48:51 +0900

    save the instances of Cube model in class variable

--- Cube.py
+++ Cube.py
@@ -1,10 +1,8 @@
 import random
 
 class Cube:
+    _all = set() # This is for managing cube instances. Can you think of the way to handle the lifecycle of cubes??
     def __init__(self, coor):
-        self.coor = [coor]
+        self.coor = coor
         self.value = random.choice([2,4])
-
-cube = Cube([1,0])
-print(cube.coor)
-print(cube.value)
+        self.__class__._all.add(self)
