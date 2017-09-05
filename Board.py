5381f5761ee87634b43fb2940cbbe49cafb67ff1
YUMIMAC <yumimac@YUMIDEV.local>
2017-09-05 18:44:20 +0900

    call Cube class in Board class and try instantiating Board. fix typo(Please be aware of it)

--- Board.py
+++ Board.py
@@ -1,24 +1,41 @@
 import random
+from Cube import Cube
 
 class Board:
+    _board_size = 5
     def __init__(self):
-        self.grid = []
+        # self.grid = [[,,,,],[,,,,],[,,,,],[,,,,],[,,,,]]
+        self.grid = [[]]
 
     # get random coor for new cube
-    def random_coor_generator(direction):
+    def random_coor_generator(self, direction):
+        print(direction)
+        coor = None # You may want to change the name of this variable
         try:
-            if direction = 'up':
+            if direction == 'up':
                 coor = [
-                    random.randint(0, board_size-1), 0]
-            elif direction = 'down':
+                    random.randint(0, self._board_size-1), 0]
+            elif direction == 'down':
                 coor = [
-                    random.randint(0, board_size-1), board_size-1)]
-            elif direction = 'left'
+                    random.randint(0, self._board_size-1), self._board_size-1]
+            elif direction == 'left':
                 coor = [
-                    board_size-1, random.randint(0, board_size-1)]
-            elif direction = 'right'
+                    self._board_size-1, random.randint(0, self._board_size-1)]
+            elif direction == 'right':
                 coor = [
-                    0, random.randint(0, board_size-1)]
+                    0, random.randint(0, self._board_size-1)]
         except ValueError:
             return random_coor_generator(input())
         return coor
+
+    # create a new cube and assign coor value
+    def create_cube(self, direction):
+        cube = Cube(self.random_coor_generator(direction))
+        print(cube.coor)
+        print(cube.value)
+        print(cube._all)
+
+
+board = Board()
+
+board.create_cube(input())
