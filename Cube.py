fc36708e2a179e7852821dbf39736eba8309bf42
YUMIMAC <yumimac@YUMIDEV.local>
2017-09-06 12:39:50 +0900

    remove unnecessary code

--- Cube.py
+++ Cube.py
@@ -1,18 +1,15 @@
 import random
 
 class Cube:
-    _all = set() # This is for managing cube instances. Can you think of the way to handle the lifecycle of cubes??
-    _target_value = 8
+    _target_value = 32
     def __init__(self, coor):
         self.coor = coor
         self.value = random.choice([2,4])
-        self.__class__._all.add(self)
 
     def change_value(self):
         self.value *= 2
 
     def tells_winning_or_not(self):
-        print("*"*80)
         if self.value == self._target_value:
             print("$"*80)
             print("You got {}. Congratulations!!!".format(self._target_value))
