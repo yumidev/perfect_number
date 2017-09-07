6fc3fe58a22c3338df91758c178c9fdb8fe84695
YUMIMAC <yumimac@YUMIDEV.local>
2017-09-07 17:05:19 +0900

    make only the main function has direction list

--- Board.py
+++ Board.py
@@ -3,7 +3,6 @@ from Cube import Cube
 
 class Board:
     _board_size = 3
-    directions = {'up': 'u', 'down': 'd', 'left': 'l', 'right': 'r'}
     def __init__(self):
         self.grid = []
         for i in range(self._board_size):
@@ -13,37 +12,37 @@ class Board:
             self.grid.append(row_list)
 
     # get random coor for new cube
-    def random_coor_generator(self, direction):
+    def random_coor_generator(self, direction, directions):
         end_index = self._board_size-1
-        if direction == self.directions['up']:
+        if direction == directions['up']:
             coor = [
                 end_index, random.randint(0, end_index)]
-        elif direction == self.directions['down']:
+        elif direction == directions['down']:
             coor = [
                 0, random.randint(0, end_index)]
-        elif direction == self.directions['left']:
+        elif direction == directions['left']:
             coor = [
                 random.randint(0, end_index), end_index]
-        elif direction == self.directions['right']:
+        elif direction == directions['right']:
             coor = [
                 random.randint(0, end_index), 0]
         return coor
 
     # create a new cube and assign coor value to the cube's value
-    def create_cube(self, direction):
+    def create_cube(self, direction, directions):
         # Make sure the space is empty
         coor = None # You may want to change the name of this variable
         count = 0 # don't try to put a new cube if you can't find a space from one wall
         while count < self._board_size:
-            coor = self.random_coor_generator(direction)
+            coor = self.random_coor_generator(direction, directions)
             count += 1
             if not self.grid[coor[0]][coor[1]]:
                 break
         if count < self._board_size:
             self.grid[coor[0]][coor[1]] = Cube(coor)
 
-    def move_cube(self, direction):
-        if direction == self.directions['up']:
+    def move_cube(self, direction, directions):
+        if direction == directions['up']:
             for i in range(self._board_size):
                 for j in range(1, self._board_size):
                     if self.grid[j][i]:
@@ -62,7 +61,7 @@ class Board:
                                 self.grid[j][i] = False
                                 if self.grid[j-1][i].tells_winning_or_not():
                                     return 1
-        elif direction == self.directions['down']:
+        elif direction == directions['down']:
             for i in range(self._board_size):
                 for j in range(self._board_size-2, -1, -1):
                     if self.grid[j][i]:
@@ -81,7 +80,7 @@ class Board:
                                 self.grid[j][i] = False
                                 if self.grid[j+1][i].tells_winning_or_not():
                                     return 1
-        elif direction == self.directions['left']:
+        elif direction == directions['left']:
             for i in range(self._board_size):
                 for j in range(1, self._board_size):
                     if self.grid[i][j]:
@@ -100,7 +99,7 @@ class Board:
                                 self.grid[i][j] = False
                                 if self.grid[i][j-1].tells_winning_or_not():
                                     return 1
-        elif direction == self.directions['right']:
+        elif direction == directions['right']:
             for i in range(self._board_size):
                 for j in range(self._board_size-2, -1, -1):
                     if self.grid[i][j]:
